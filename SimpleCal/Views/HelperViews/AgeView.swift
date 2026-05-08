d2a4fb109cc3007b168564413f7bc6caf2a88f15
JakeAtwood24 <4jake.atwood@gmail.com>
2026-05-08 03:42:35 -0400

    Completely revamped the StartView file, adding inputs that are visually more interesting. Created a basic food model as well for future use. Saved user inputs.

---
+++ SimpleCal/Views/HelperViews/AgeView.swift
@@ -0,0 +1,18 @@
+//
+//  AgeView.swift
+//  SimpleCal
+//
+//  Created by Jake Atwood on 5/7/26.
+//
+
+import SwiftUI
+
+struct AgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgeView()
+}
---
+++ SimpleCal/Views/HelperViews/BarSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  BarSelectView.swift
+//  SimpleCal
+//
+//  Created by Jake Atwood on 5/7/26.
+//
+
+import SwiftUI
+
+struct BarSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarSelectView()
+}
