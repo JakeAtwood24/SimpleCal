a82420b33502461b32b5accbfd38f3fcb8f0eae3
JakeAtwood24 <4jake.atwood@gmail.com>
2026-05-10 17:48:06 -0400

    Created a view to show the amount of calories eaten in a day out of the total. (Need to fix where it gets the values from)

---
+++ SimpleCal/Views/HelperViews/CalorieWheelView.swift
@@ -0,0 +1,18 @@
+//
+//  CalorieWheelView.swift
+//  SimpleCal
+//
+//  Created by Jake Atwood on 5/10/26.
+//
+
+import SwiftUI
+
+struct CalorieWheelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalorieWheelView()
+}
