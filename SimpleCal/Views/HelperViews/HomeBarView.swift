7c566df9c2914885c65659dab42081f8cf10ab62
JakeAtwood24 <4jake.atwood@gmail.com>
2026-05-09 21:16:21 -0400

    Carried over streak and home bar views from a previous App (will modify further).

---
+++ SimpleCal/Views/HelperViews/HomeBarView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeBarView.swift
+//  SimpleCal
+//
+//  Created by Jake Atwood on 5/9/26.
+//
+
+import SwiftUI
+
+struct HomeBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeBarView()
+}
